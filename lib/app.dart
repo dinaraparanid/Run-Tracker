a61b0330cad867fd5044a8b74c825cc739bb223d
Arseny Savchenko <58735614+dinaraparanid@users.noreply.github.com>
2024-09-21 11:30:54 +0300

    universal-platform-support (#3)

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:run_tracker/core/ui/theme/app.dart';
+import 'package:universal_platform/universal_platform.dart';
 
 class App extends ConsumerWidget {
   const App({super.key});
@@ -10,12 +12,48 @@ class App extends ConsumerWidget {
     final theme = ref.watch(appThemeProvider);
     final backgroundColor = theme.colors.background.primary;
 
+    // TODO: final home = ...
+
+    if (UniversalPlatform.isAndroid) {
+      return materialWidget(
+          theme: theme,
+          // TODO: home screen
+          home: Scaffold(
+            backgroundColor: backgroundColor,
+            body: const Text("TODO: Home Screen"),
+          )
+      );
+    }
+
+    if (UniversalPlatform.isIOS) {
+      return cupertinoWidget(
+          theme: theme,
+        // TODO: home screen
+          home: CupertinoPageScaffold(
+            backgroundColor: backgroundColor,
+            child: const Text("TODO: Home Screen"),
+          ),
+      );
+    }
+
+    return Container();
+  }
+
+  Widget materialWidget({required AppTheme theme, required Widget home}) {
+    final backgroundColor = theme.colors.background.primary;
+
+    return MaterialApp(
+      color: backgroundColor,
+      home: home,
+    );
+  }
+
+  Widget cupertinoWidget({required AppTheme theme, required Widget home}) {
+    final backgroundColor = theme.colors.background.primary;
+
     return CupertinoApp(
       color: backgroundColor,
-      home: CupertinoPageScaffold(
-          backgroundColor: backgroundColor,
-          child: Text("TODO: Home Screen")
-      ),
+      home: home,
     );
   }
 }
--- lib/feature/sprint/presentation/screen.dart
+++ lib/feature/sprint/presentation/screen.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:run_tracker/core/ui/theme/app.dart';
 import 'package:run_tracker/core/ui/theme/images.dart';
-
-const _cancelIconSize = 48.0;
+import 'package:universal_platform/universal_platform.dart';
 
 class SprintScreen extends ConsumerWidget {
   const SprintScreen({super.key});
@@ -11,20 +11,40 @@ class SprintScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final theme = ref.watch(appThemeProvider);
+
+    if (UniversalPlatform.isAndroid) return materialWidget(theme);
+    if (UniversalPlatform.isIOS) return cupertinoWidget(theme);
+    return Container();
+  }
+
+  Widget materialWidget(AppTheme theme) {
+    final backgroundColor = theme.colors.background.primary;
+
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      appBar: AppBar(
+        backgroundColor: backgroundColor,
+        leading: cancelIcon(theme),
+      ),
+      body: Container(),
+    );
+  }
+
+  Widget cupertinoWidget(AppTheme theme) {
     final backgroundColor = theme.colors.background.primary;
 
     return CupertinoPageScaffold(
       backgroundColor: backgroundColor,
       navigationBar: CupertinoNavigationBar(
         backgroundColor: backgroundColor,
-        leading: Image(
-          image: AssetImage(AppImages.load('ic_cross.png')),
-          width: _cancelIconSize,
-          height: _cancelIconSize,
-          color: theme.colors.button.topBar,
-        ),
+        leading: cancelIcon(theme),
       ),
       child: Container(),
     );
   }
+
+  Widget cancelIcon(AppTheme theme) => Image(
+    image: AssetImage(AppImages.load('ic_cross.png')),
+    color: theme.colors.button.topBar,
+  );
 }
